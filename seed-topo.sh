1e91e53023c504ade325dd5a0296ad0218b8a2f0
Stephan Zaat <stephan@zosc.nl>
2016-03-16 16:18:02 +0100

    OPS-29 Mapproxy naar Docker

--- seed-topo.sh
+++ seed-topo.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-mapproxy-seed /app/seed.yaml -f /app/mapproxy.yaml --seed=topo_rd
-## && mapproxy-seed /app/seed.yaml -f /app/mapproxy.yaml --seed=topo_rd_detail
+mapproxy-seed /app/seed.yaml -f /app/mapproxy.yaml --seed=topo_rd && mapproxy-seed /app/seed.yaml -f /app/mapproxy.yaml --seed=topo_rd_detail
