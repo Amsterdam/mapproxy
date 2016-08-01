a7971b45d1470f41b5eaf9e0a7fe5bdc1b267e9d
Stephan Zaat <stephan@zosc.nl>
2016-08-01 14:53:02 +0200

    TG-881 New mapproxy settings

--- seed-bgt.sh
+++ seed-bgt.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-#mapproxy-seed -c 6 -s /app/seed.yaml -f /app/mapproxy.yaml --seed=topo_rd_bgt,topo_google_bgt
-mapproxy-seed -c 6 -s /app/seed.yaml -f /app/mapproxy.yaml --seed=topo_google_bgt
+mapproxy-seed -c 6 -s /app/seed.yaml -f /app/mapproxy.yaml --seed=topo_rd_bgt,topo_google_bgt
