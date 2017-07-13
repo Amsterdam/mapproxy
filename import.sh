97c77e1c9e99f6db05a069debcb31e41dece62e2
Stephan Zaat <s.zaat@amsterdam.nl>
2017-07-13 13:12:32 +0200

    Adjust mapproxy for style layers

--- import.sh
+++ import.sh
@@ -13,15 +13,15 @@ dc() {
 
 # start database
 dc up -d --build database
-sleep 10
+#sleep 10
 
 # create dirs
 sudo mkdir -p /mnt/tiles
 sudo chmod 755 /mnt/tiles
 
 # import basiskaart db
-dc exec database update-db.sh basiskaart
-dc exec database update-db.sh atlas
+#dc exec database update-db.sh basiskaart
+#dc exec database update-db.sh atlas
 
 # generate geojson
 #dc run --rm topo_rd
