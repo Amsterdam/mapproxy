989af66e169f2fa7a2245ec10391050e3fa282e1
Stephan Zaat <s.zaat@amsterdam.nl>
2017-07-17 13:06:54 +0200

    TG-757 Enable local tiling based on Basiskaart

--- import.sh
+++ import.sh
@@ -9,21 +9,20 @@ dc() {
 	docker-compose -p mapproxy -f ${DIR}/docker-compose.yml $*
 }
 
-#trap 'dc kill ; dc rm -f' EXIT
+trap 'dc kill ; dc rm -f' EXIT
 
 # start database
 dc up -d --build database
-#sleep 10
+sleep 10
 
 # create dirs
 sudo mkdir -p /mnt/tiles
 sudo chmod 755 /mnt/tiles
 
 # import basiskaart db
-#dc exec database update-db.sh basiskaart
-#dc exec database update-db.sh atlas
+dc exec database update-db.sh basiskaart
+dc exec database update-db.sh atlas
 
 # generate geojson
-#dc run --rm topo_rd
 dc build
 dc run topo_rd
