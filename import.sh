687603f826588be03110854dac098b9051c7b00e
sana <s.benyerrou@amsterdam.nl>
2017-07-18 11:42:07 +0200

    TG-757 Ready for jenkins

--- import.sh
+++ import.sh
@@ -6,7 +6,7 @@ set -u
 DIR="$(dirname $0)"
 
 dc() {
-	docker-compose -p mapproxy -f ${DIR}/docker-compose.yml $*
+	docker-compose -p mapproxy -f ${DIR}/docker-compose.yml $* -T
 }
 
 trap 'dc kill ; dc rm -f' EXIT
