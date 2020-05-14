c96c6c388bf397caec934d70f5848693dc253ae1
Nick Garanko <ngaranko@gmail.com>
2020-05-14 15:59:08 +0200

    Use acceptance for tile generation.

--- start_tiles.sh
+++ start_tiles.sh
@@ -32,6 +32,4 @@ dc exec -T database update-db.sh bag_v11
 
 # generate geojson
 dc build
-docker cp src/indexes.sh "$(dc ps -q database)":/tmp/indexes.sh
-dc exec database psql -U basiskaart -d basiskaart -f /tmp/indexes.sh
 dc run topo_$1
