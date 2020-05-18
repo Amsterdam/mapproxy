61a68d2cfc86a5fb14f44fc63d9812411e432aa6
Nick Garanko <ngaranko@gmail.com>
2020-05-18 15:57:41 +0200

    Verbosity.

--- start_tiles.sh
+++ start_tiles.sh
@@ -6,14 +6,17 @@ set -u
 DIR="$(dirname $0)"
 
 dc() {
-	docker-compose pull
+	# docker-compose pull
 	docker-compose -p mapproxy -f ${DIR}/docker-compose.yml $*
 }
 
 trap 'dc kill ; dc rm -f' EXIT
 
+echo "Kill"
 dc kill
+echo "Remove"
 dc rm -f
+echo "Stop"
 # clean environment
 dc down --remove-orphans
 
@@ -21,9 +24,10 @@ dc down --remove-orphans
 # dc up -d --force-recreate database
 # Start mapserver
 # dc up -d mapserver
-sleep 10
+# sleep 10
 
 # create dirs
+echo "Create dirs"
 sudo mkdir -p /mnt/tiles
 sudo chmod 755 /mnt/tiles
 sudo rm -rf /mnt/tiles/*
@@ -33,6 +37,7 @@ sudo rm -rf /mnt/tiles/*
 # dc exec -T database update-db.sh bag_v11
 
 
+echo "Build"
 # generate geojson
 dc build
 dc run topo_$1
