f692895062351d21c5aa98b567aaa24f0345cb07
Edward Mac Gillavry <edward@webmapper.net>
2023-09-18 16:41:03 +0200

    Only build 1 image and allocate RAM.

--- start_tiles.sh
+++ start_tiles.sh
@@ -27,6 +27,6 @@ echo "Cleanup old cache /mnt/tiles/topo_$1_cache_EPSG28992"
 sudo rm -rf "/mnt/tiles/topo_$1_cache_EPSG28992/"
 
 echo "Build"
-# generate geojson
-dc build
-dc run topo_$1
+# generate tiles
+dc build topo_$1
+dc run topo_$1 --memory="8g" --memory-swap="32g"
