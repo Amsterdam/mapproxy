b68605b36e06bdf17c7f1bfaf22e46d87b2a79fa
Edward Mac Gillavry <edward@webmapper.net>
2024-11-29 16:46:48 +0100

    Remove db and update compose syntax.

--- start_tiles.sh
+++ start_tiles.sh
@@ -6,8 +6,8 @@ set -u
 DIR="$(dirname $0)"
 
 dc() {
-	docker-compose pull
-	docker-compose -p mapproxy -f ${DIR}/docker-compose.yml $*
+	docker compose pull
+	docker compose -p mapproxy -f ${DIR}/compose.yml $*
 }
 
 trap 'dc kill ; dc rm -f' EXIT
@@ -15,8 +15,6 @@ trap 'dc kill ; dc rm -f' EXIT
 # clean environment
 dc down
 
-# start database
-dc up -d --force-recreate database
 # Start mapserver
 dc up -d mapserver
 sleep 10
@@ -25,13 +23,5 @@ sleep 10
 sudo mkdir -p /mnt/tiles
 sudo chmod 755 /mnt/tiles
 
-# import basiskaart db
-dc exec -T database update-db.sh basiskaart
-dc exec -T database update-db.sh bag_v11
-
-
-# generate geojson
 dc build
-docker cp src/indexes.sh "$(dc ps -q database)":/tmp/indexes.sh
-dc exec database psql -U basiskaart -d basiskaart -f /tmp/indexes.sh
 dc run topo_$1
