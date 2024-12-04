88192b78b90a54165afbced4a34277183332f36d
Edward Mac Gillavry <edward@webmapper.net>
2024-12-04 13:43:57 +0100

    Don't save tiles locally, but add them straight to Azure Blob Store.

--- start_tiles.sh
+++ start_tiles.sh
@@ -7,21 +7,13 @@ DIR="$(dirname $0)"
 
 dc() {
 	docker compose pull
-	docker compose -p mapproxy -f ${DIR}/compose.yml $*
+	docker compose -p mapproxy -f ${DIR}/compose-seed.yml $*
 }
 
 trap 'dc kill ; dc rm -f' EXIT
 
 # clean environment
 dc down
-
-# Start mapserver
-dc up -d mapserver
-sleep 10
-
-# create dirs
-sudo mkdir -p /mnt/tiles
-sudo chmod 755 /mnt/tiles
 
 dc build
 dc run topo_$1
